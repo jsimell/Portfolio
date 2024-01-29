46677d948528861d009c71b4be98b01d2a5f5e71
Jiri Simell <jiri.simell@gmail.com>
2024-01-29 14:23:54 +0200

    Reconstructed the routing algorithm

--- app.js
+++ app.js
@@ -12,76 +12,80 @@ async function handler(req) {
     const filename = path.split("/").pop();
     const directory = path.slice(0, path.lastIndexOf("/") + 1);
 
+    // Handling of GET requests
+    if (method ==  "GET") {
 
-    // Get information on the requested resource
-    try {
-        const fileInfo = await Deno.stat(`${Deno.cwd()}${path}`);
-        //console.log(fileInfo);
-    } catch (error) {
-        console.error(`Error reading file info for ${path}:`, error);
-    }
+        // Get information on the requested resource
+        let fileInfo = {};
+        try {
+            fileInfo = await Deno.stat(`${Deno.cwd()}${path}`);
+            console.log(fileInfo);
+        } catch (error) {
+            console.error(`Error reading file info for ${path}:`, error);
+        }
+ 
+        if (fileInfo.isDirectory) {
 
-
-    // GET /
-    if (method == "GET" && path == "/") {
-        const htmlContent = await Deno.readTextFile("index.html");
-        return new Response(htmlContent, {
-            headers: {
-                "content-type": "text/html; charset=utf-8",
+            // GET /
+            if (path == "/") {
+                const htmlContent = await Deno.readFile(`${Deno.cwd()}/index.html`);
+                return new Response(htmlContent, {
+                    headers: {
+                        "content-type": "text/html; charset=utf-8",
+                    }
+                });
             }
-        });
-    }
 
-    // GET images/CV_photo.png
-    if (method == "GET" && path == "/images/cv_photo.jpg") {
-        const img = await Deno.readFile("images/cv_photo.jpg");
-        return new Response(img, {
-            headers: {
-                "content-type": "image/jpeg",
+        } else if (fileInfo.isFile) {
+
+            // Read the requested file
+            const file = await Deno.readFile(`${Deno.cwd()}${path}`);
+
+            if (/\.html$/.test(filename)) {
+                return new Response(file, {
+                    headers: {
+                        "content-type": "text/html",
+                    }
+                });
+            } else if (/\.css$/.test(filename)) {
+                return new Response(file, {
+                    headers: {
+                        "content-type": "text/css",
+                    }
+                });
+            } else if (/\.js$/.test(filename)) {
+                return new Response(file, {
+                    headers: {
+                        "content-type": "text/javascript",
+                    }
+                });
+            } else if (/\.jpg$/.test(filename)) {
+                return new Response(file, {
+                    headers: {
+                        "content-type": "image/jpg",
+                    }
+                });
+            } else if (/\.png$/.test(filename)) {
+                return new Response(file, {
+                    headers: {
+                        "content-type": "image/png",
+                    }
+                });
+            } else if (/\.ico$/.test(filename)) {
+                return new Response(file, {
+                    headers: {
+                        "content-type": "image/vnd.microsoft.icon",
+                    }
+                });
+            } else if (/\.ico$/.test(filename)) {
+                return new Response(file, {
+                    headers: {
+                        "content-type": "image/vnd.microsoft.icon",
+                    }
+                });
             }
-        });
+        }
     }
-
-    // GET css/styles.css
-    if (method == "GET" && path == "/styles.css") {
-        const cssFile = await Deno.readTextFile("styles.css");
-        return new Response(cssFile, {
-            headers: {
-                "content-type": "text/css",
-            }
-        });
-    }
-
-    // GET includeScript.js
-    if (method == "GET" && path == "/includeScript.js") {
-        const script = await Deno.readFile("includeScript.js");
-        return new Response(script, {
-            headers: {
-                "content-type": "text/javascript",
-            }
-        });
-    }
-
-    // GET /templates/*
-    if (method == "GET" && templates.includes(filename)) {
-        const htmlContent = await Deno.readTextFile(`templates/${filename}`);
-        return new Response(htmlContent, {
-            headers: {
-                "content-type": "text/html; charset=utf-8",
-            }
-        });
-    }
-
-    // GET favicon.ico
-    if (method == "GET" && path == "/favicon.ico") {
-        const icon = await Deno.readFile("favicon.ico");
-        return new Response(icon, {
-            headers: {
-                "content-type": "image/vnd.microsoft.icon",
-            }
-        });
-    }
-
 }
 
 const port = 8000
