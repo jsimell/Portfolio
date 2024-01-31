1a48d850994cfae1966e1352e2ba3b1a0b71f541
Jiri Simell <jiri.simell@gmail.com>
2024-01-31 11:35:00 +0200

    Created css and js folders for css files and js scripts

---
+++ js/includeScript.js
@@ -0,0 +1,9 @@
+// This script can be used to include html files into other html files
+const includes = document.getElementsByTagName("include");
+Array.from(includes).forEach(async include => {
+    // Load the file from the path defined in the attributes of the include element
+    const res = await fetch(include.getAttribute("src"));
+    include.insertAdjacentHTML('afterend', await res.text());
+    include.remove(); // removes the 'include' element from the document as it is no longer needed after the content has been included
+});
+console.log("includeScript.js executed.");
