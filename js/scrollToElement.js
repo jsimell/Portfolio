167530eed20538cc27c61c345213489db61715e9
Jiri Simell <jiri.simell@gmail.com>
2024-02-13 23:10:12 +0200

    Reduced the scroll amount

--- js/scrollToElement.js
+++ js/scrollToElement.js
@@ -4,7 +4,7 @@ function scrollToElement(elementId) {
     const element = document.getElementById(elementId);
     const headerHeight = document.getElementById("header").getBoundingClientRect().bottom;
     if (element) {
-        const yPos = element.getBoundingClientRect().top + window.scrollY - headerHeight - 60;
+        const yPos = element.getBoundingClientRect().top + window.scrollY - headerHeight - 30;
         window.scrollTo({
             top: yPos,
             behavior: 'smooth'
